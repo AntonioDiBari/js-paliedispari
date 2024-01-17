0a2cf35d7e4f9685388e6304d56b67ebcfa8902a
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-17 17:58:57 +0100

    feat: generazione numero randomico + somma

--- es Pari e Dispari/js/functions.js
+++ es Pari e Dispari/js/functions.js
@@ -8,11 +8,33 @@ function askNumber() {
   let userNumberValid = false;
   while (!userNumberValid) {
     userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
-    if (userNumber > 0 && userNumber < 5) {
+    if (userNumber > 0 && userNumber <= 5 && !isNaN(userNumber)) {
       userNumberValid = true;
     } else {
       alert("Inserisci un numero corretto");
     }
   }
   return userNumber;
+}
+/**
+ * Crea un numero randomico tra un numero max e un numero min
+ * @param {number} min Il valore minimo da cui partire a generare
+ * @param {number} max Il valore massimo a cui può arrivare a generare
+ * @returns Numero randomico
+ */
+function getRandomNumber(min, max) {
+  min = parseInt(min);
+  max = parseInt(max);
+
+  if (isNaN(min) || isNaN(max)) {
+    console.error("Inserisci solo dei numeri!");
+    return;
+  }
+
+  if (min >= max) {
+    console.error("Il numero massimo deve esser più grande del numero minimo!");
+    return;
+  }
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
 }
--- es Pari e Dispari/js/main.js
+++ es Pari e Dispari/js/main.js
@@ -1,11 +1,18 @@
 const evenOrodd = document.getElementById("even-odd");
 const startBtn = document.getElementById("start");
-let userNumber = "";
+const numMin = 1;
+const numMax = 5;
 
 startBtn.addEventListener("click", function () {
   let userChoise = evenOrodd.value;
-  console.log(userChoise);
+  console.log("Scelta dall'utente: ", userChoise);
 
-  userNumber = askNumber();
-  console.log(userNumber);
+  let userNumber = askNumber();
+  console.log("Numero scelto dall'utente: ", userNumber);
+
+  let computerNumber = getRandomNumber(numMin, numMax);
+  console.log("Il numero generato per il computer è: ", computerNumber);
+
+  let sum = userNumber + computerNumber;
+  console.log("La somma dei due numeri è: ", sum);
 });
