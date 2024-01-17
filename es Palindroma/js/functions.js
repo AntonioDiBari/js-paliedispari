2a05d9ac1983dc99e3672b11976e66145928c507
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-01-17 16:11:42 +0100

    feat: js palindroma con functions

--- es Palindroma/js/functions.js
+++ es Palindroma/js/functions.js
@@ -1,3 +1,39 @@
 //**  FUNCTIONS
 
-/** */
+/**
+ * Chiedo la parola
+ * @returns parola richiesta
+ */
+function askWord() {
+  let isValid = false;
+  let userWord = "";
+  while (!isValid) {
+    isValid = true;
+    userWord = prompt(
+      "Inserisci una parola per verificare che sia palindroma"
+    ).trim();
+    for (i = userWord.length - 1; i >= 0; i--) {
+      let char = userWord.charAt(i);
+      console.log(char);
+      if (!isNaN(char)) {
+        isValid = false;
+        alert("Non inserire numeri!");
+      }
+    }
+  }
+
+  return userWord;
+}
+
+/**
+ * Prende ogni lettera di una parola da invertire e la monta in una variabile con un ciclo for
+ * @returns Parola al contrario
+ */
+function reverseWord() {
+  let reverseWord = "";
+  for (i = userWord.length - 1; i >= 0; i--) {
+    let reverseLetter = userWord.charAt(i);
+    reverseWord += reverseLetter;
+  }
+  return reverseWord;
+}
--- es Palindroma/js/main.js
+++ es Palindroma/js/main.js
@@ -1,1 +1,16 @@
-console.log("js OK");
+const result = document.getElementById("result");
+
+// CHIEDO LA PAROLA
+let userWord = askWord();
+console.log(userWord);
+
+// MONTO LA PAROLA AL CONTRARIO
+let reversedWord = reverseWord();
+console.log(reversedWord);
+
+// CONTRONTO LE PAROLE
+if (userWord === reversedWord) {
+  result.innerText = `La parola ${userWord} è palindorma.`;
+} else {
+  result.innerText = `La parola ${userWord} non è palindorma, in quanto ${userWord} non è uguale a ${reversedWord}.`;
+}
